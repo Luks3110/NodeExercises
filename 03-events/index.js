91ac00f91999f3031e144fd03af9fc3a73b1638a
Luks3110 <lucas_bugado@hotmail.com>
2022-02-07 16:59:52 -0300

    events

---
+++ 03-events/index.js
@@ -0,0 +1,31 @@
+const EventEmitter = require('events')
+class MeuEmissor extends EventEmitter{
+
+}
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+meuEmissor.on(nomeEvento, function(click){
+    console.log(`Um usuário clicou ${click}`)
+})
+
+// meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+// meuEmissor.emit(nomeEvento, 'no ok')
+
+// let count = 0;
+// setInterval(function(){
+//     meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+
+// }, 1000)
+
+const stdin = process.openStdin()
+
+function main(){
+    return new Promise(function(resolve, reject){
+        stdin.addListener('data', function(value){
+            return resolve(value)
+        })
+    })
+}
+main().then(function(resultado){
+    console.log('resultado', resultado.toString())
+})
